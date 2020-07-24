46296807db3712065c8af640d4fb578ed2752d0a
minhee <queen.minhee@gmail.com>
2020-07-25 01:23:50 +0900

    [ADD] props  & prop-types 적용

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PropTypes from "prop-types";
+
+function Food({ fav, picture, rating }) {
+  return (
+    <div>
+      <h2>I love {fav}</h2>
+      <h4>{rating}/5.0</h4>
+      <img src={picture} alt={fav} />
+    </div>
+  );
+}
+
+const foodLike = [
+  {
+    id: 1,
+    name: "Choco",
+    rating: 4.5,
+    image: "https://ifh.cc/g/IvrS1y.jpg",
+  },
+  {
+    id: 2,
+    name: "Coffee",
+    rating: 3,
+    image: "https://ifh.cc/g/smboPX.jpg",
+  },
+  {
+    id: 3,
+    name: "Pasta",
+    rating: 4,
+    image: "https://ifh.cc/g/fzlVRf.jpg",
+  },
+  {
+    id: 4,
+    name: "Pizza",
+    rating: 4.8,
+    image: "https://ifh.cc/g/roVY2P.jpg",
+  },
+];
+
+Food.propTypes = {
+  fav: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number,
+};
+
+// function renderFood(dish) {
+//   return <Food fav={dish.name} picture={dish.image} />;
+// }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello!</h1>
+      {foodLike.map((dish) => (
+        <Food
+          key={dish.id}
+          fav={dish.name}
+          picture={dish.image}
+          rating={dish.rating}
+        />
+      ))}
     </div>
   );
 }
