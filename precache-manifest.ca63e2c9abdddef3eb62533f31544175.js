a58bca2aab66f3d35994aedf4c04fd8e0d38022f
minhee <queen.minhee@gmail.com>
2020-07-29 17:06:18 +0900

    Updates

---
+++ precache-manifest.ca63e2c9abdddef3eb62533f31544175.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "142f7701001adcda3c9859825dda659a",
+    "url": "/movie-app-2020/index.html"
+  },
+  {
+    "revision": "a8d19a4d09638722a7eb",
+    "url": "/movie-app-2020/static/css/main.2d72fa2a.chunk.css"
+  },
+  {
+    "revision": "7fed2ceb7f8d8fb15280",
+    "url": "/movie-app-2020/static/js/2.34815e04.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie-app-2020/static/js/2.34815e04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8d19a4d09638722a7eb",
+    "url": "/movie-app-2020/static/js/main.53495397.chunk.js"
+  },
+  {
+    "revision": "b03465f25580502d820c",
+    "url": "/movie-app-2020/static/js/runtime-main.3e7f16a7.js"
+  }
+]);
